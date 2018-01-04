c522dc03c1aed510ad543c9bb7210514389a6403
Swati Jain <swati@jain.se>
2018-01-04 13:46:48 +0100

    Add project files.

---
+++ Palindrom/Palindrom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Palindrom
+    {
+
+        public bool FindPalindrom(String inputString)
+        {
+            String str =  inputString.Replace(" ", "").ToUpper();
+            int length = str.Length;
+            for (int i=0; i<length/2; i++)
+            {
+                if (str[i] != str[length - i -1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Palindrom/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string to check if palindrome:");
+            String input = Console.ReadLine();
+
+            Palindrom pal = new Palindrom();
+
+            if (pal.FindPalindrom(input))
+                Console.WriteLine("Your string is Palindrome!");
+            else
+                Console.WriteLine("Your string is NOT Palindrome");
+        }
+    }
+}
